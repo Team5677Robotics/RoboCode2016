c761c7cedc9195b33f95a9fa1a5a05e33a2c8b5e
Vedaad Shakib <vedaad799@gmail.com>
2016-02-10 20:47:37 -0800

    Added RobotPorts.java

---
+++ src/org/usfirst/frc/team5677/robot/RobotPorts.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team5677.robot;
+
+/*
+ * victor sp 1 - cim - 0
+ * victor sp 2 - cim - 0
+ * victor sp 3 - cim - 1
+ * victor sp 4 - cim - 1
+ * victor sp 5 - manipulator - 2
+ * spark 1 - turret control - 3
+ * spark 2 - manipulator - 4
+ * talon 1 - manipulator - 5
+ * talon 2 - manipulator - 6
+ * talon 3 - manipulator - 7
+ * talon 4 - manipulator - 8
+
+ * 6  8-inch wheels, 3 on each side, 2 motors on each side controlled by 2 victor sps each (connected with y ables)
+ */
+
+/*
+ * Maps each motor to a pwm input port.
+ *
+ * @author Vedaad Shakib
+ * @version 2/10/16
+ */
+public class RobotPorts {
+    public static final int DT_VICTORSP_L_PORT = 0;
+    public static final int DT_VICTORSP_R_PORT = 1;
+}
