aa94672bd0206db917d857587a804a0f5b43f7e6
Vedaad Shakib <vedaad799@gmail.com>
2016-02-11 22:37:32 -0800

    Added static methods to DriveTrain

--- src/org/usfirst/frc/team5677/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5677/robot/subsystems/DriveTrain.java
@@ -17,8 +17,11 @@ public class DriveTrain extends Subsystem {
     private static VictorSP victorLeft;
     private static VictorSP victorRight;
     
-    // scale the speed
+    // speed scale
     private static final double SCALE = 1;
+
+    // singleton instance of DriveTrain
+    private static DriveTrain driveTrain;
 
     public void initDefaultCommand() {
     }
@@ -30,10 +33,42 @@ public class DriveTrain extends Subsystem {
     	victorLeft = new VictorSP(RobotMap.DT_VICTORSP_L_PORT);
     	victorRight = new VictorSP(RobotMap.DT_VICTORSP_R_PORT);
     }
+
+    /*
+     * Initializes a (singleton) instance of the drive train if none exists.
+     *
+     * @postcondition initializes singleton instance of drive train.
+     */
+    public static void initialize() {
+	if (driveTrain == null) {
+	    driveTrain = new DriveTrain();
+	}
+    }
+
+    public static DriveTrain getInstance() {
+    	initialize();
+    	return driveTrain;
+    }
     
+    /*
+     * Sets the speed of VictorSPs that control the left and right side.
+     * 
+     * @param left the speed of the left VictorSP (-1 <= right <= 1)
+     * @param right the speed of the right VictorSP (-1 <= left <= 1)
+     */
     public void setSpeed(double left, double right) {
     	victorRight.set(left);
     	victorLeft.set(right);
+    }
+    
+    /*
+     * Sets the speed of VictorSPs that control the left and right side.
+     * 
+     * @param speed the speed of both VictorSPs (-1 <= speed <= 1)
+     */
+    public void setSpeed(double speed) {
+    	victorRight.set(speed);
+    	victorLeft.set(speed);
     }
 }
 
